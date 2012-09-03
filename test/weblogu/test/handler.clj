f72041e8114e08a8d6e8ff189ea5f4b7b8fc0537
Basavanagowda Kanur <basavanagowda@gmail.com>
2012-09-03 23:07:25 +0530

    Refactor handler tests using 'testing' macro from clojure.test

--- test/weblogu/test/handler.clj
+++ test/weblogu/test/handler.clj
@@ -4,16 +4,26 @@
         [ring.mock.request :only [request]]))
 
 (deftest test-index
-  (is (map? (handler (request :get "/"))))
-  (is (= (:status (handler (request :get "/")) 200)))
-  (is (not (nil? (:body (handler (request :get "/"))))))
-  (is (map? (handler (request :post "/"))))
-  (is (= (:status (handler (request :post "/"))) 405)))
+  (testing "/"
+    (testing "GET method."
+      (let [r (handler (request :get "/"))]
+        (is (map? r) "should return a map.")
+      (is (= (:status r) 200) "should have status code 200.")
+      (is (not (nil? (:body r))) "body should not be null.")))
+    (testing "POST method."
+      (let [r (handler (request :post "/"))]
+        (is (map? r) "should return a map.")
+        (is (= (:status r) 405) "should have status code 405.")))))
 
 (deftest test-add
-  (is (map? (handler (request :get "/add"))))
-  (is (= (:status (handler (request :get "/add"))) 200))
-  (is (not (nil? (:body (handler (request :get "/add"))))))
-  (is (map? (handler (request :post "/add"))))
-  (is (= (:status (handler (request :post "/add"))) 302))
-  (is (map? (:headers (handler (request :post "/add"))))))
+  (testing "/add"
+    (testing "GET method."
+      (let [r (handler (request :get "/add"))]
+        (is (map? r) "should return a map.")
+        (is (= (:status r) 200) "should have status code 200.")
+        (is (not (nil? (:body r))) "body should not be null.")))
+    (testing "POST method."
+      (let [r (handler (request :post "/add"))]
+        (is (map? r) "should return a map.")
+        (is (= (:status r) 302) "should have status code 302.")
+        (is (map? (:headers r)) "should return a map of headers.")))))
