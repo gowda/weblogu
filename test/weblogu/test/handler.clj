1a13bb68e24c0d89ead5c42d71505018a67fc56d
Basavanagowda Kanur <basavanagowda@gmail.com>
2012-09-08 13:51:05 +0530

    Add tests for checking the presence of links on navbar

--- test/weblogu/test/handler.clj
+++ test/weblogu/test/handler.clj
@@ -1,7 +1,22 @@
 (ns weblogu.test.handler
+  (:require [net.cgrand.enlive-html :as enlive])
   (:use [weblogu.handler]
         [clojure.test]
-        [ring.mock.request :only [request]]))
+        [ring.mock.request :only [request body]]))
+
+(defn- string->dom-tree [string]
+  (with-open [s (-> (java.io.StringReader. string) java.io.BufferedReader.)]
+    (enlive/html-resource s)))
+
+(defn- links-on-navbar [body]
+  (let [link-nodes (enlive/select (string->dom-tree body)
+                                  [:div.navbar :div.container :ul :li :a])]
+    (map #(first (enlive/attr-values % :href)) link-nodes)))
+
+(defn- navbar-healthy? [body]
+  (let [links #{"/" "/add" "/list"}
+        on-navbar (links-on-navbar body)]
+    (every? links on-navbar)))
 
 (defn test-ring-success [response]
   (is (map? response) "should return a map.")
@@ -12,7 +27,8 @@
   (testing "/"
     (testing "GET method."
       (let [r (handler (request :get "/"))]
-        (test-ring-success r)))
+        (test-ring-success r)
+        (is (navbar-healthy? (:body r)) "navbar should contain expected links")))
     (testing "POST method."
       (let [r (handler (request :post "/"))]
         (is (map? r) "should return a map.")
@@ -22,12 +38,21 @@
   (testing "/add"
     (testing "GET method."
       (let [r (handler (request :get "/add"))]
-        (test-ring-success r)))
-    (testing "POST method."
-      (let [r (handler (request :post "/add"))]
-        (is (map? r) "should return a map.")
-        (is (= (:status r) 302) "should have status code 302.")
-        (is (map? (:headers r)) "should return a map of headers.")))))
+        (test-ring-success r)
+        (is (navbar-healthy? (:body r)) "navbar should contain expected links")))
+    (testing "POST method"
+      ;; (testing "without any form data"
+      ;;   (let [r (handler (request :post "/add"))]
+      ;;     (test-ring-success r)
+      ;;     (is (navbar-healthy? (:body r)) "navbar should contain expected links")
+      ;;     (is (form-with-error? (:body r)))))
+      (testing "with form data"
+        (let [v {:title "Blog entry title from tests"
+                 :body "Blog entry body from tests"}
+              r (handler (body (request :post "/add") v))]
+          (is (map? r) "should return a map.")
+          (is (= (:status r) 302) "should have status code 302.")
+          (is (map? (:headers r)) "should return a map of headers."))))))
 
 (deftest test-list
   (testing "/list"
