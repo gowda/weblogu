ffcafbf512fb07c87d503d3c41eb5c46b38ccf12
Basavanagowda Kanur <basavanagowda@gmail.com>
2012-09-08 17:42:42 +0530

    Use view and data storage procedures from newly created ns.

--- src/weblogu/handler.clj
+++ src/weblogu/handler.clj
@@ -1,43 +1,18 @@
 (ns weblogu.handler
-  (:require [net.cgrand.enlive-html :as enlive])
-  (:use [ring.util.response :only [response redirect]]
+  (:require [weblogu.blog :as blog])
+  (:use [weblogu.model :only [load-entries]]
+        [ring.util.response :only [response redirect]]
         [ring.middleware.params :only [wrap-params]]
         [ring.middleware.keyword-params :only [wrap-keyword-params]]
         [ring.middleware.resource :only [wrap-resource]]
         [net.cgrand.moustache :only [app]]))
 
-(def ^:private posts (atom []))
-
-(enlive/defsnippet post "templates/index.html" [:ul#posts]
-  [post]
-  [:li.title] (enlive/content (:title post)))
-
-(enlive/deftemplate layout "templates/_layout.html" [content]
-  [:div#content-main] (enlive/content content))
-
-(enlive/defsnippet index "templates/index.html" [:div#index] [posts]
-  [:div#posts] (enlive/content (map post posts)))
-
-(enlive/defsnippet add "templates/add.html" [:form#add] [])
-
-(defn save-posts []
-  (spit "posts.db" (pr-str @posts)))
-
-(defn load-posts []
-  (swap! posts (fn [old new] new) (read-string (slurp "posts.db"))))
-
-(defn add-post [{:keys [title body] :as post}]
-  (swap! posts conj post)
-  (save-posts))
-
 (def handler
   (app (wrap-resource "public") wrap-params wrap-keyword-params
-       [""] {:get (fn [req] (response (apply str (layout (index @posts)))))}
-       ["add"] {:get (fn [req] (response (apply str (layout (add)))))
-                :post (fn [req]
-                        (add-post (:params req))
-                        (redirect "/"))}
-       ["list"] {:get (fn [req] (response (apply str (layout (map post @posts)))))}))
+       [""] {:get (fn [req] (blog/home-get (:params req)))}
+       ["add"] {:get (fn [req] (blog/add-get (:params req)))
+                :post (fn [req] (blog/add-post (:params req)))}
+       ["list"] {:get (fn [req] (blog/list-get (:params req)))}))
 
 (defn init []
-  (load-posts))
+  (load-entries))
